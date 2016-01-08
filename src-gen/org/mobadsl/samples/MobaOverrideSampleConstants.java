af861de8937a3758f2d9ae49c372ba0cca22abf1
Florian Pirchner <florian.pirchner@gmail.com>
2016-01-08 17:33:38 +0100

    Copyright

--- src-gen/org/mobadsl/samples/MobaOverrideSampleConstants.java
+++ src-gen/org/mobadsl/samples/MobaOverrideSampleConstants.java
@@ -1,13 +1,3 @@
-/**
- * Copyright (c) 2015 - 2016, Lunifera GmbH (Wien), Ekkehard-Gentz (Rosenheim)
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *         Florian Pirchner - Initial implementation
- */
 package org.mobadsl.samples;
 
 /**
