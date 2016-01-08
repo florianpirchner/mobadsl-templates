43997542fe552806767087c0bd20e9744477cd82
Florian Pirchner <florian.pirchner@gmail.com>
2016-01-08 16:55:39 +0100

    Added copyrights

--- src-gen/org/mobadsl/templates/MobaMimeTypesTemplateConstants.java
+++ src-gen/org/mobadsl/templates/MobaMimeTypesTemplateConstants.java
@@ -1,3 +1,13 @@
+/**
+ * Copyright (c) 2015 - 2016, Lunifera GmbH (Wien), Ekkehard-Gentz (Rosenheim)
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *         Florian Pirchner - Initial implementation
+ */
 package org.mobadsl.templates;
 
 /**
@@ -94,7 +104,7 @@ public interface MobaMimeTypesTemplateConstants {
 	  * 		@valueType = CONSTANT
 	  * 		</p>
 	 */ 
-	public static final String Const__CONTENTTYPE_TEXT_PLAIN = "text/plain";
+	public static final String Const__CONTENTTYPE_TEXT_PLAIN = Const__MIMETYPE__TEXT_PLAIN;
 	
 	/**
 	  * Content is a JPEG Image<p>@value = <code>image/jpeg</code><br>
